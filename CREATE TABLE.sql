c64979221e8f55f6e760904c1e955b210a3da16d
Aleksandr <GReddyd1990@gmail.com>
2021-08-24 21:28:53 +0300

    Change CREATE TABLE. Add DB and Schema DB

--- CREATE TABLE.sql
+++ CREATE TABLE.sql
@@ -1,4 +1,7 @@
--- Создание таблицы
-CREATE TABLE PERSONS (PRIMARYKEY(name VARCHAR(256), surname VARCHAR(256), age INT), 
-					  phone_number VARCHAR(15), 
-					  city_of_living VARCHAR(256));
+-- Создание таблицы БД lk - личный кабинет, Схема БД public;
+CREATE TABLE lk.public.PERSONS (name VARCHAR(256),
+                      surname VARCHAR(256),
+                      age INT,
+					  phone_number VARCHAR(15),
+					  city_of_living VARCHAR(256),
+					  PRIMARY KEY (name, surname, age));
